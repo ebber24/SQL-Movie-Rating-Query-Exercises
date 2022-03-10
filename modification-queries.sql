fefa38b66d5084a654740aab297e55ccd322c007
ebber24 <47796859+ebber24@users.noreply.github.com>
2022-03-10 18:43:12 +0100

    Create modification-queries.sql

---
+++ modification-queries.sql
@@ -0,0 +1,25 @@
+-- Q01 Add the reviewer Roger Ebert to your database, with an rID of 209.
+
+INSERT INTO Reviewer
+VALUES      (209,
+             "Roger Ebert") 
+             
+-- Q02 For all movies that have an average rating of 4 stars or higher, add 25 to the release year. (Update the existing tuples; don't insert new tuples.)
+
+UPDATE movie
+SET    year = year + 25
+WHERE  mid IN (SELECT mID
+               FROM   movie
+                      INNER JOIN rating using (mID)
+               GROUP  BY title
+               HAVING Avg(stars) >= 4) 
+
+-- Q02 Remove all ratings where the movie's year is before 1970 or after 2000, and the rating is fewer than 4 stars.
+
+DELETE FROM Rating
+WHERE  mID IN(SELECT mID
+              FROM   movie
+              WHERE  year > 1970
+                      OR year > 2000)
+       AND stars < 4; 
+       
